47618ec7ff67e0c095601206ce1d3c1449d95c43
Ana Paola Treviño <anapao.trevino@gmail.com>
2019-12-01 17:30:43 -0600

    change file capital letter

--- src/controllers/events.js
+++ src/controllers/events.js
@@ -1,4 +1,4 @@
-const Event = require('../models/Event')
+const Event = require('../models/event')
 
 const getEvents = function(req, res) {
     Event.find({ createdBy: req.user._id }).then(function(events) {
